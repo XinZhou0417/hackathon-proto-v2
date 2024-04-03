2ed2314076102477ff727375c459b8d7f5f31972
xZhou <xinzhou0417@gmail.com>
2024-04-04 00:33:35 +1100

    Add log4j logger

--- src/main/java/team/gpt2p5/hackathonprotospringboot/controller/UserController.java
+++ src/main/java/team/gpt2p5/hackathonprotospringboot/controller/UserController.java
@@ -4,6 +4,8 @@ package team.gpt2p5.hackathonprotospringboot.controller;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,11 @@ import team.gpt2p5.hackathonprotospringboot.rest.HttpStatusCode;
 @RequestMapping("/user")
 public class UserController {
 
+    static final Logger logger = LogManager.getLogger(UserController.class);
+
     @GetMapping("/user/{userId}")
     public AwsProxyResponse getUserByID(@PathVariable String userId) throws JsonProcessingException {
+        logger.info("entered getUserByID method");
         User user = new User(userId, "neo", "neo@matrix.com");
         String body = new ObjectMapper().writeValueAsString(user);
         AwsProxyResponse response = new AwsProxyResponse(HttpStatusCode.SUCCESS, null, body);
