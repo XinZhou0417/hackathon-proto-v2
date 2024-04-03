7fcd021285c7ea514cb0f770d0a50c15f2ae83bf
xZhou <xinzhou0417@gmail.com>
2024-04-03 22:01:09 +1100

    init

---
+++ src/main/java/team/gpt2p5/hackathonprotospringboot/HackathonProtoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package team.gpt2p5.hackathonprotospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonProtoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonProtoSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/gpt2p5/hackathonprotospringboot/HackathonProtoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package team.gpt2p5.hackathonprotospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonProtoSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
